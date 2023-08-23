78dbea87a3fb29cd53fe1422f01868deff1ee5c4
PernillePersson <78405936+PernillePersson@users.noreply.github.com>
2023-08-23 10:50:02 +0200

    Bog/låner klasser oprettet

---
+++ Bibliotek/Bog.cs
@@ -0,0 +1,11 @@
+namespace Bibliotek;
+
+public class Bog
+{
+    public int id { get; set; }
+    public String titel { get; set; }
+    public String år { get; set; }
+    public String forlag { get; set; }
+    public bool udlånt { get; set; }
+    
+}
---
+++ Bibliotek/FileReader.cs
@@ -0,0 +1,6 @@
+namespace Bibliotek;
+
+public class FileReader
+{
+    
+}
