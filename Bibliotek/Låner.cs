c97be0eb057aaa84416c669dddd522de6ce58611
PernillePersson <78405936+PernillePersson@users.noreply.github.com>
2023-08-23 10:57:56 +0200

    Bog/Låner klasser oprettet

---
+++ Bibliotek/Låner.cs
@@ -0,0 +1,10 @@
+﻿namespace Bibliotek;
+
+public class Låner
+{
+    public int id { get; set; }
+    public String navn { get; set; }
+    public String adresse { get; set; }
+    public float tlf { get; set; }
+    public List<Bog> lånt { get; set; }
+}
